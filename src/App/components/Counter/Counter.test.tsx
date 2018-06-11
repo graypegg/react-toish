5a9340c92b8b11afc599d918e77db07f9654ef1c
Graham Pegg <toish@toi.sh>
2018-06-10 21:36:33 -0400

    Oops, bad test

--- src/App/components/Counter/Counter.test.tsx
+++ src/App/components/Counter/Counter.test.tsx
@@ -2,6 +2,10 @@ import * as React from 'react'
 import { shallow } from 'enzyme'
 import Counter from './Counter'
 
-it('renders without crashing', () => {
-  shallow(<Counter />)
+it('renders a number when showNumber=true', () => {
+  shallow(<Counter showNumber />)
+})
+
+it('does not render a number when showNumber=false', () => {
+  shallow(<Counter showNumber={false} />)
 })
