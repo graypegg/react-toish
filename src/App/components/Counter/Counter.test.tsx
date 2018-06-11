7c516fdfff096d64a0ed455b3f3f74a244107b77
Graham Pegg <toish@toi.sh>
2018-06-10 22:43:22 -0400

    figured out how to test components wrapped in a hoc

--- src/App/components/Counter/Counter.test.tsx
+++ src/App/components/Counter/Counter.test.tsx
@@ -2,10 +2,14 @@ import * as React from 'react'
 import { shallow } from 'enzyme'
 import Counter from './Counter'
 
+import { store } from 'store'
+
 it('renders a number when showNumber=true', () => {
-  shallow(<Counter showNumber />)
+  const component = shallow(<Counter showNumber store={store} />).dive()
+  expect(component.find('h1').text()).toBe('10')
 })
 
 it('does not render a number when showNumber=false', () => {
-  shallow(<Counter showNumber={false} />)
+  const component = shallow(<Counter showNumber={false} store={store} />).dive()
+  expect(component.find('h1').text()).toBe('?')
 })
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,13 +1,17 @@
 import { configure } from 'enzyme'
 import * as Adapter from 'enzyme-adapter-react-16'
+import configureStore from 'redux-mock-store'
 
-(global as any).Date.now = jest.fn(() => 0)
+declare const global: any
 
-const localStorageMock = {
+global.Date.now = jest.fn(() => 0)
+
+global.store = configureStore
+
+global.localStorage = {
   getItem: jest.fn(),
   setItem: jest.fn(),
   clear: jest.fn()
-};
-(global as any).localStorage = localStorageMock 
+}
 
 configure({ adapter: new Adapter() })
